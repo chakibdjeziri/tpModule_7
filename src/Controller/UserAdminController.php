a18136388f843db08a7cc953ba8cf367cd7d2bee
djeziri <ch.djeziri@gmail.com>
2024-02-22 15:22:38 +0100

    Adding edit password's admin forms

--- src/Controller/UserAdminController.php
+++ src/Controller/UserAdminController.php
@@ -55,42 +55,46 @@ class UserAdminController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
     #[Route('/utilisateur/edition-mot-de-passe/{id}', 'user.edit.password', methods: ['GET', 'POST'])]
     public function editPassword(
-            UserAdmin $User,
-            Request $request,
-            EntityManagerInterface $manager,
-            UserPasswordHasherInterface $hasher
-        ): Response {
-            $form = $this->createForm(AdminPasswordType::class);
-    
-            $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid()) {
-                if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
-                    $User->setDateCreation(new \DateTimeImmutable());
-                    $User->setPlainPassword(
-                        $form->getData()['newPassword']
-                    );
-    
-                    $this->addFlash(
-                        'success',
-                        'Le mot de passe a été modifié.'
-                    );
-    
-                    $manager->persist($User);
-                    $manager->flush();
-    
-                    return $this->redirectToRoute('app_admin');
-                } else {
-                    $this->addFlash(
-                        'warning',
-                        'Le mot de passe renseigné est incorrect.'
-                    );
-                }
+        UserAdmin $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher
+    ): Response {
+        $form = $this->createForm(AdminPasswordType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setDateCreation(new \DateTimeImmutable());
+                $user->setPlainPassword(
+                    $form->getData()['newPassword']
+                );
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été modifié.'
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                return $this->redirectToRoute('app_admin');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect.'
+                );
             }
-    
-            return $this->render('/user_admin/edit_password.html.twig', [
-                'form' => $form->createView()
-            ]);
+        }
+
+        return $this->render('/user_admin/edit_password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
+
+
--- src/Form/AdminPasswordType.php
+++ src/Form/AdminPasswordType.php
@@ -16,39 +16,39 @@ class AdminPasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('plainPassword', RepeatedType::class, [
-            'type' => AdminPasswordType::class,
-            'first_options' => [
-                'attr' => [
-                    'class' => 'form-control'
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Ancien mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label  mt-4'
+                    ]
                 ],
-                'label' => 'Mot de passe',
-                'label_attr' => [
-                    'class' => 'form-label  mt-4'
-                ]
-            ],
-            'second_options' => [
-                'attr' => [
-                    'class' => 'form-control'
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Confirmation de l\'ancien mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label  mt-4'
+                    ]
                 ],
-                'label' => 'Confirmation du mot de passe',
-                'label_attr' => [
-                    'class' => 'form-label  mt-4'
-                ]
-            ],
-            'invalid_message' => 'Les mots de passe ne correspondent pas.'
-        ])
-        ->add('newPassword', AdminPasswordType::class, [
-            'attr' => ['class' => 'form-control'],
-            'label' => 'Nouveau mot de passe',
-            'label_attr' => ['class' => 'form-label mt-4'],
-            'constraints' => [new Assert\NotBlank()]
-        ])
-        ->add('submit', SubmitType::class, [
-            'attr' => [
-                'class' => 'btn btn-primary mt-4'
-            ],
-            'label' => 'Changer mon mot de passe'
-        ]);
-    }
+                'invalid_message' => 'Les mots de passe ne correspondent pas.'
+                ])
+                ->add('newPassword', PasswordType::class, [
+                    'attr' => ['class' => 'form-control'],
+                    'label' => 'Nouveau mot de passe',
+                    'label_attr' => ['class' => 'form-label mt-4'],
+                    'constraints' => [new Assert\NotBlank()]
+                ])
+                ->add('submit', SubmitType::class, [
+                    'attr' => [
+                        'class' => 'btn btn-primary m-4'
+                    ],
+                    'label' => 'Changer mon mot de passe'
+                ]);
+        }
 }
